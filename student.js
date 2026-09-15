02174b8aa4697bcb097c08ab0bcec50108b9ec1a
Afsana <begumafsanayasmin2009@gmali,com>
2026-09-15 23:45:39 +0530

    initial

---
+++ student.js
@@ -0,0 +1,15 @@
+let studentname="Afsana";
+let age=18;
+let course="bca";
+const collegename="sitm";
+let marks=70;
+let birthyear=2008;
+studentname="simran"
+marks=90;
+
+console.log(studentname);
+console.log(age);
+console,log(course);
+console.log(collegename);
+console.log(marks);
+console.log(birthyear);
